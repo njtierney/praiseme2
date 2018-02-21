752811a4dbf542f810f53ed0b791daa12b310817
Nicholas Tierney <nicholas.tierney@gmail.com>
2018-02-22 10:14:41 +1100

    first commit! :tada:

---
+++ R/praise.R
@@ -0,0 +1,11 @@
+#' @title Praise your user
+#'
+#' @description This function delivers praise the user when they type "praise()".
+#'
+#' @examples
+#' praise()
+#'
+#' @export
+praise <- function(){
+  paste0("You are awesome!!!")
+}
